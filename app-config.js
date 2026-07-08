81a7aebb74ad55e478b9f7055e4aaa90d21aec74
FAISAL ALANAZI <faialanazi@ksu.edu.sa>
2026-07-08 09:26:18 +0300

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -7,3 +7,9 @@ window.FIREBASE_CONFIG = {
   messagingSenderId: "117900186571",
   appId: "1:117900186571:web:a128800f2c13497944dc4e"
 };
+
+// Google Apps Script Web App URL for paper evaluation upload
+window.DRIVE_UPLOAD_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwgQweSIvkOU5ALLbAWte9of-EWPDcGM4tQw9uC6NxmjEIRciO073UCK7n_qhrvBxAegA/exec";
+
+// Require paper evaluation file before submission
+window.REQUIRE_PAPER_FILE_UPLOAD = true;
